acd35e30a68d99ffdf6e997e89bef9965eff9f20
Imtiaz <146869794+imtiaz135@users.noreply.github.com>
2026-01-02 23:46:28 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+place = input("Where are u from? ")
+
+print("\nhey! Welcome ")
+print("Hello", name + "!")
+print("You are", age, "years old.")
+print("so you are from", place + "!")
+print("it's my pleasure to meet you! ")
